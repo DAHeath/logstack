100e10d64f2b2cbfb852dfbf4cd6399dc6a2d000
David Heath <heath.davidanthony@gmail.com>
2020-09-20 19:28:48 -0400

    implementation skeleton

---
+++ build/Test_include-b858cb2.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/Users/david/Documents/MPC/blogb-stacked-garbling/build/Test_tests-b858cb2.cmake")
+  include("/Users/david/Documents/MPC/blogb-stacked-garbling/build/Test_tests-b858cb2.cmake")
+else()
+  add_test(Test_NOT_BUILT-b858cb2 Test_NOT_BUILT-b858cb2)
+endif()
