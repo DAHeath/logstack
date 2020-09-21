aabe1ff6857a229782a52bafd9e985af4d1ebd01
David Heath <heath.davidanthony@gmail.com>
2020-09-21 11:04:44 -0400

    working AND gates

--- src/main.cc
+++ src/main.cc
@@ -1,207 +1,37 @@
-#include <vector>
-#include <variant>
-#include <memory>
-#include <bitset>
-#include <span>
-
-template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
-template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
-
-enum class GateType { INPUT, OUTPUT, AND, XOR };
-
-
-struct Gate {
-  GateType type;
-  std::size_t inp0, inp1, out;
-};
-
-
-using Netlist = std::vector<Gate>;
-
-
-struct Circuit;
-
-
-struct Conditional {
-  std::vector<Circuit> cs;
-};
-
-
-using Sequence = std::vector<Circuit>;
-
-
-struct Circuit {
-  std::variant<Netlist, Conditional, Sequence> content;
-  std::size_t nWires;
-  std::size_t nInp;
-  std::size_t nOut;
-  std::size_t nRow;
-};
-
-
-
-using Label = std::bitset<128>;
-using Labelling = std::vector<Label>;
-
-
-struct Encoding {
-  Labelling zeros;
-  Label delta;
-};
-
-
-using Material = std::vector<Label>;
-
-
-struct Garbling {
-  Encoding inputEncoding;
-  Encoding outputEncoding;
-  Material material;
-};
-
-
-using Wiring = std::vector<Label>;
-
-
-void gbGate(
-    const Gate& g,
-    Wiring& w,
-    const Label& delta,
-    std::span<Label>& material,
-    std::span<const Label>& inp,
-    std::span<Label>& out) {
-  switch (g.type) {
-    case GateType::INPUT:
-      // read in the next label from the input labelling
-      w[g.out] = inp[0];
-      inp = inp.subspan(1);
-      break;
-    case GateType::OUTPUT:
-      // write out to the end of the output labelling
-      out[0] = w[g.inp0];
-      out = out.subspan(1);
-      break;
-    case GateType::AND:
-      // TODO
-      break;
-    case GateType::XOR:
-      w[g.out] = w[g.inp0] ^ w[g.inp1];
-      break;
-  }
-}
-
-
-void evGate(
-    const Gate& g,
-    Wiring& w,
-    std::span<Label>& material,
-    std::span<const Label>& inp,
-    std::span<Label>& out) {
-  switch (g.type) {
-    case GateType::INPUT:
-      // read in the next label from the input labelling
-      w[g.out] = inp[0];
-      inp = inp.subspan(1);
-      break;
-    case GateType::OUTPUT:
-      // write out to the end of the output labelling
-      out[0] = w[g.inp0];
-      out = out.subspan(1);
-      break;
-    case GateType::AND:
-      // TODO
-      break;
-    case GateType::XOR:
-      w[g.out] = w[g.inp0] ^ w[g.inp1];
-      break;
-  }
-}
-
-Garbling garble(const Circuit&, const Label& seed);
-
-Encoding gb(const Circuit& c, const Encoding& inputEncoding, std::span<Label>& material) {
-  return std::visit(overloaded {
-    [&](const Netlist& n) {
-      const auto delta = inputEncoding.delta;
-      Encoding outputEncoding;
-      outputEncoding.delta = delta;
-      outputEncoding.zeros.resize(c.nOut);
-
-      Wiring wiring(c.nWires);
-      std::span<const Label> inps(inputEncoding.zeros);
-      std::span<Label> outs(outputEncoding.zeros);
-
-      for (const auto& g: n) {
-        gbGate(g, wiring, delta, material, inps, outs);
-      }
-      return outputEncoding;
-    },
-    [&](const Conditional& cond) {
-      Encoding e;
-
-      // TODO gen demux
-      // TODO gen mux
-      std::vector<Garbling> gs;
-      gs.reserve(cond.cs.size());
-      for (const auto& c: cond.cs) {
-        gs.push_back(garble(c, 0)); // TODO properly define seed based on condition
-      }
-      return e;
-    },
-    [&](const Sequence& seq) {
-      auto encoding = inputEncoding;
-      for (const auto& c: seq) {
-        encoding = gb(c, encoding, material);
-      }
-      return encoding;
-    },
-  }, c.content);
-}
-
-
-Garbling garble(const Circuit& c, const Label& seed) {
-  Garbling g;
-  // TODO generate by pulling from PRG
-  // g.inputEncoding.delta = PRG
-  // g.inputEncoding[0] = 1;
-  // g.inputEncoding.zeros = std::generate 
-
-  g.material.resize(c.nRow);
-  std::span<Label> mat(g.material);
-  g.outputEncoding = gb(c, g.inputEncoding, mat);
-  return g;
-}
-
-
-Labelling ev(const Circuit& c, const Labelling& input, std::span<Label>& material) {
-  return std::visit(overloaded {
-    [&](const Netlist& n) {
-      Labelling output(c.nOut);
-
-      Wiring wiring(c.nWires);
-      std::span<const Label> inps(input);
-      std::span<Label> outs(output);
-
-      for (const auto& g: n) {
-        evGate(g, wiring, material, inps, outs);
-      }
-      return output;
-    },
-    [&](const Conditional& cond) {
-      Labelling out;
-      // TODO
-      return out;
-    },
-    [&](const Sequence& seq) {
-      auto labelling = input;
-      for (const auto& c: seq) {
-        labelling = ev(c, labelling, material);
-      }
-      return labelling;
-    },
-  }, c.content);
-}
-
+#include "scheme.h"
+#include <iostream>
 
 int main() {
+
+  Circuit c {
+    Netlist {
+      Gate { GateType::INPUT, 0, 0, 0 },
+      Gate { GateType::INPUT, 0, 0, 1 },
+      Gate { GateType::AND, 0, 1, 2 },
+      Gate { GateType::OUTPUT, 2, 0 , 0 },
+    },
+    3, // nWires
+    2, // nInp
+    1, // nOut
+    2, // nRow
+  };
+
+
+  PRG seed;
+  PRF k;
+
+  auto g = garble(seed, k, c);
+
+  const auto delta = g.inputEncoding.delta;
+
+  const Labelling inp = {
+    g.inputEncoding.zeros[0],
+    g.inputEncoding.zeros[1],
+  };
+  std::span<Label> mat(g.material);
+  const auto out = ev(k, c, inp, mat);
+
+
+  std::cout << (out[0] == g.outputEncoding.zeros[0]) << '\n';
+  std::cout << (out[0] == (g.outputEncoding.zeros[0] ^ delta)) << '\n';
 }
---
+++ src/scheme.cc
@@ -0,0 +1,184 @@
+#include "scheme.h"
+
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+
+void gbGate(
+    const PRF& f,
+    const Gate& g,
+    const Label& delta,
+    NetlistCtxt& ctxt) {
+
+  const auto& A0 = ctxt.w[g.inp0];
+  const auto& B0 = ctxt.w[g.inp1];
+  auto& C0 = ctxt.w[g.out];
+  switch (g.type) {
+    case GateType::INPUT:
+      // read in the next label from the input labelling
+      C0 = ctxt.inp[0];
+      ctxt.inp = ctxt.inp.subspan(1);
+      break;
+
+    case GateType::OUTPUT:
+      // write out to the end of the output labelling
+      ctxt.out[0] = A0;
+      ctxt.out = ctxt.out.subspan(1);
+      break;
+
+    case GateType::AND: {
+      const auto hA0 = f(A0);
+      const auto hA1 = f(A0 ^ delta);
+      const auto hB0 = f(B0);
+      const auto hB1 = f(B0 ^ delta);
+      const auto A0D = A0 & delta;
+      const auto B0D = B0 & delta;
+
+      const auto X = A0[0] ? (hA1 ^ B0D) : hA0;
+      const auto Y = B0[0] ? (hB1 ^ A0D) : hB0;
+
+      ctxt.material[0] = hA0 ^ hA1 ^ B0D;
+      ctxt.material[1] = hB0 ^ hB1 ^ A0D;
+
+      ctxt.material = ctxt.material.subspan(2);
+
+      C0 = A0&B0 ^ X ^ Y;
+      break;
+    }
+
+    case GateType::XOR:
+      C0 = A0 ^ B0;
+      break;
+  }
+}
+
+
+void evGate(
+    const PRF& f,
+    const Gate& g,
+    NetlistCtxt& ctxt) {
+  const auto& A = ctxt.w[g.inp0];
+  const auto& B = ctxt.w[g.inp1];
+  auto& C = ctxt.w[g.out];
+  switch (g.type) {
+    case GateType::INPUT:
+      // read in the next label from the input labelling
+      C = ctxt.inp[0];
+      ctxt.inp = ctxt.inp.subspan(1);
+      break;
+
+    case GateType::OUTPUT:
+      // write out to the end of the output labelling
+      ctxt.out[0] = A;
+      ctxt.out = ctxt.out.subspan(1);
+      break;
+
+    case GateType::AND: {
+      const auto hA = f(A);
+      const auto hB = f(B);
+      const auto X = A[0] ? hA ^ ctxt.material[0] : hA;
+      const auto Y = B[0] ? hB ^ ctxt.material[1] : hB;
+      ctxt.material = ctxt.material.subspan(2);
+      C = (A&B) ^ X ^ Y;
+      break;
+    }
+
+    case GateType::XOR:
+      C = A ^ B;
+      break;
+  }
+}
+
+Encoding gb(const PRF& f, const Circuit& c, const Encoding& inputEncoding, std::span<Label>& material) {
+  return std::visit(overloaded {
+    [&](const Netlist& n) {
+      const auto delta = inputEncoding.delta;
+      Encoding outputEncoding;
+      outputEncoding.delta = delta;
+      outputEncoding.zeros.resize(c.nOut);
+
+      NetlistCtxt ctxt;
+      ctxt.w = Wiring(c.nWires);
+      ctxt.material = material;
+      ctxt.inp = std::span<const Label>(inputEncoding.zeros);
+      ctxt.out = std::span<Label>(outputEncoding.zeros);
+
+      for (const auto& g: n) {
+        gbGate(f, g, delta, ctxt);
+      }
+
+      material = ctxt.material;
+
+      return outputEncoding;
+    },
+    [&](const Conditional& cond) {
+      Encoding e;
+
+      // TODO gen demux
+      // TODO gen mux
+      std::vector<Garbling> gs;
+      gs.reserve(cond.cs.size());
+      for (const auto& c: cond.cs) {
+        PRG prg { 0 };
+        gs.push_back(garble(prg, f, c)); // TODO properly define seed based on condition
+      }
+      return e;
+    },
+    [&](const Sequence& seq) {
+      auto encoding = inputEncoding;
+      for (const auto& c: seq) {
+        encoding = gb(f, c, encoding, material);
+      }
+      return encoding;
+    },
+  }, c.content);
+}
+
+
+Garbling garble(PRG& seed, const PRF& f, const Circuit& c) {
+  Garbling g;
+  g.inputEncoding.delta = seed();
+  g.inputEncoding.delta[0] = 1;
+  g.inputEncoding.zeros.resize(c.nInp);
+  for (auto& z: g.inputEncoding.zeros) { z = seed(); }
+
+  g.material.resize(c.nRow);
+  std::span<Label> mat(g.material);
+  g.outputEncoding = gb(f, c, g.inputEncoding, mat);
+  return g;
+}
+
+
+Labelling ev(const PRF& f, const Circuit& c, const Labelling& input, std::span<Label>& material) {
+  return std::visit(overloaded {
+    [&](const Netlist& n) {
+      Labelling output(c.nOut);
+
+      NetlistCtxt ctxt;
+      ctxt.w = Wiring(c.nWires);
+      ctxt.material = material;
+      ctxt.inp = std::span<const Label>(input);
+      ctxt.out = std::span<Label>(output);
+
+      for (const auto& g: n) {
+        evGate(f, g, ctxt);
+      }
+
+      material = ctxt.material;
+      return output;
+    },
+    [&](const Conditional& cond) {
+      Labelling out;
+      // TODO
+      return out;
+    },
+    [&](const Sequence& seq) {
+      auto labelling = input;
+      for (const auto& c: seq) {
+        labelling = ev(f, c, labelling, material);
+      }
+      return labelling;
+    },
+  }, c.content);
+}
