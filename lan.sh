23440540e34f13fe0ea6d434ec32ba5f87fa05fb
David Heath <heath.davidanthony@gmail.com>
2021-11-24 21:12:11 -0500

    publish

--- lan.sh
+++
@@ -1,4 +0,0 @@
-brew services restart shopify/shopify/toxiproxy
-toxiproxy-cli create SLOW --listen localhost:55555 -u localhost:55556
-toxiproxy-cli toxic add SLOW -t latency -a latency=2
-toxiproxy-cli toxic add SLOW -t bandwidth -a rate=125000
--- src/main.cc
+++ src/main.cc
@@ -31,10 +31,6 @@ double experiment(const Circuit& c) {
     }));
   }
 
-  /* std::cout << (genChannel->counter + evalChannel->counter) << '\n'; */
-  /* genChannel->counter = 0; */
-  /* evalChannel->counter = 0; */
-
   return std::accumulate(results.begin(), results.end(), 0.0)/results.size();
 }
 
@@ -46,28 +42,6 @@ int main(int argc, char** argv) {
     sha.desc.nOut,
     sha.desc.nRow,
   };
-  /* std::vector<Circuit> cs = { */
-  /*   sha_netlist, sha_netlist, */
-  /*   sha_netlist, sha_netlist, */
-  /*   sha_netlist, sha_netlist, */
-  /*   sha_netlist, sha_netlist, */
-  /* }; */
-  /* std::cout << experiment(conditional(cs)) << '\n'; */
-
-
-  /* std::vector<Circuit> cs = { sha_netlist }; */
-  /* for (std::size_t i = 0; i <= 10; ++i) { */
-  /*   if (cs.size() == 1) { */
-  /*     std::cout << experiment(cs[0]) << '\n'; */
-  /*   } else { */
-  /*     std::cout << experiment(conditional(cs)) << '\n'; */
-  /*   } */
-
-  /*   for (std::size_t j = 0; j < (1 << i); ++j) { */
-  /*     cs.push_back(sha_netlist); */
-  /*   } */
-  /* } */
-
 
   std::thread th { [&] {
     genChannel = new emp::NetIO(nullptr, 44444, true);
@@ -76,75 +50,13 @@ int main(int argc, char** argv) {
   evalChannel = new emp::NetIO("127.0.0.1", 44444, true);
   th.join();
 
-
-  /* int n = atoi(argv[1]); */
-
-
-  /* std::vector<Circuit> cs; */
-  /* for (std::size_t i = 0; i < n; ++i) { */
-  /*   cs.push_back(sha_netlist); */
-  /* } */
-  /* if (cs.size() == 1) { */
-  /*   /1* std::cout << experiment(cs[0]) << '\n'; *1/ */
-  /*   experiment(cs[0]); */
-  /* } else { */
-  /*   /1* std::cout << experiment(conditional(cs)) << '\n'; *1/ */
-  /*   experiment(conditional(cs)); */
-  /* } */
-
-
-
-  /* /1* for (std::size_t i = 0; i < 128; ++i) { *1/ */
-  /* /1*   cs.push_back(sha_netlist); *1/ */
-  /* /1* } *1/ */
-  /* /1* std::cout << experiment(conditional(cs)) << '\n'; *1/ */
-
-  /* for (std::size_t i = 0; i <= 14; ++i) { */
-    std::vector<Circuit> cs;
-    for (std::size_t j = 0; j < (1 << 4); ++j) {
-      cs.push_back(sha_netlist);
-    }
-    if (cs.size() == 1) {
-      std::cout << experiment(cs[0]) << '\n';
-      /* experiment(cs[0]); */
-    } else {
-      std::cout << experiment(conditional(cs)) << '\n';
-      /* experiment(conditional(cs)); */
-    }
-  /* } */
-
-
-/*   PRG prg; */
-/*   PRF f; */
-
-
-/*   const auto enc = genEncoding(prg, 3); */
-
-/*   const auto b = 2; */
-
-/*   std::vector<Label> seeds(2*b - 2); */
-/*   for (std::size_t i = 0; i < 2*b - 2; ++i) { */
-/*     seeds[i] = prg(); */
-/*   } */
-
-/*   Material m(40); */
-/*   for (auto& r: m) { r = 0; } */
-/*   std::span<Label> mat(m); */
-/*   const auto bad = gbGadget(b, f, enc.delta, seeds, enc.zeros, mat); */
-
-
-/*   mat = m; */
-
-/*   Labelling index = enc.zeros; */
-/*   index[0] ^= enc.delta; */
-/*   index[1] ^= enc.delta; */
-
-/*   const auto actual = evGadget(b, f, index, mat); */
-
-
-/*   for (std::size_t i = 0; i < seeds.size(); ++i) { */
-/*     show(actual[i]); */
-/*     show(seeds[i]); */
-/*     show(bad[i]); */
-/*   } */
+  std::vector<Circuit> cs;
+  for (std::size_t j = 0; j < (1 << 4); ++j) {
+    cs.push_back(sha_netlist);
+  }
+  if (cs.size() == 1) {
+    std::cout << experiment(cs[0]) << '\n';
+  } else {
+    std::cout << experiment(conditional(cs)) << '\n';
+  }
 }
